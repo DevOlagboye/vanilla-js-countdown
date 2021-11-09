0a77fbbae6858deb7925772747a48ebd442b153a
Akingbola1234 <akingbolaola@gmail.com>
2021-11-09 15:37:52 +0100

    Finally added Countdown to the button

--- script.js
+++ script.js
@@ -41,17 +41,27 @@ let interval = setInterval(function() {
 });
 
 let button = document.querySelector("#btn");
-let now = new Date().getHours();
-let hDistance = now + 6;
-
-let runCount = () => {
-    let hHours = hDistance;
-    const hSeconds = Math.floor(hDistance) % 60;
-    let hMinutes = Math.floor(hDistance / 60) % 60;
-    console.log(hHours, hMinutes, hSeconds);
+let countDown = () => {
+    // let hHours = hDistance;
+    // const hSeconds = Math.floor(hDistance) % 60;
+    // let hMinutes = Math.floor(hDistance / 60) % 60;
+    // console.log(hHours, hMinutes, hSeconds);
+    let runCount = setInterval(function() {
+        let now = new Date().getSeconds();
+        //21,600,000 miliseconds is 6hrs
+        let hDistance = 21600 - now;
+        let Hhours = Math.floor(hDistance / 3600);
+        let Hminutes = Math.floor((hDistance % 3600) / 60);
+        let Hseconds = hDistance % 60;
+        //console.log(now, hDistance, Hhours, Hminutes, Hseconds);
+        button.innerHTML = Hhours + " h " + Hminutes + " m " + Hseconds + " s ";
+        if (hDistance < 0) {
+            clearInterval(runCount);
+        }
+    }, 1000);
 };
 //setInterval(runCount, 1000);
-button.addEventListener("click", runCount);
+button.addEventListener("click", countDown);
 
 console.log(today);
 console.log(countDownDate);
